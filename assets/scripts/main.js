16dc3fe78837bbed4ba5845c63c613d79f91bd9b
Tucker Barrett <tuckerbarrett@gmail.com>
2021-07-15 19:25:06 -0400

    Creates basic layout, adds normalize.css

---
+++ assets/scripts/main.js
@@ -0,0 +1,6 @@
+let searchButton = document.querySelector("#search-button");
+
+searchButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("This is clicked");
+})
