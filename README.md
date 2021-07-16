fd64bf5694e01d9947911b3263d0e1ea36d4fc89
Tucker Barrett <tuckerbarrett@gmail.com>
2021-07-16 13:20:20 -0400

    Adds history functionality

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Weather Dashboard
-This project focuses on using a 3rd party API, specifically the Open Weather One-Call API, to create a weather dashboard to allow a user to search for their city (or any city) and see the current weather as well as a 5-day forecast.
+This project focuses on using a 3rd party API, specifically the Open Weather One-Call API, to create a weather dashboard to allow a user to search for their city (or any city) and see the current weather as well as a 5-day forecast for that city.
+
+I began by creating the layout, opting for pure CSS instead of a framework to continue honing my pure CSS skills. 
+
+Next I implemented the <code>localStorage</code> functionality, adding searched cities to the history, both in the local storage as well as on the page. This included adding event listeners to each history item, as well as enabling and disabling the "Clear History" button when appropriate.
 
 
 
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,84 @@
+// Assign variables related to search
+let searchFormEl = document.querySelector("#search-form");
+let searchInputEl = document.querySelector("#search-text");
 let searchButton = document.querySelector("#search-button");
+let clearHistoryButton = document.querySelector("#clear-history");
 
-searchButton.addEventListener("click", function(event) {
+let historyListEl = document.querySelector("#search-history-list");
+
+// Set history items from local storage if available, and to an empty array if not
+let historyItems = JSON.parse(localStorage.getItem("history") || "[]");
+let numCities = localStorage.getItem("numCities") || 0;
+
+
+searchFormEl.addEventListener("submit", function(event) {
     event.preventDefault();
-    console.log("This is clicked");
-})
+    let searchedCity = searchInputEl.value.trim();
+    if (searchedCity.length !== 0) {
+        addToHistory(searchedCity);
+    }
+});
+
+clearHistoryButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    localStorage.clear();
+    historyItems = [];
+    historyListEl.innerHTML = "";
+    clearHistoryButton.disabled = true;
+});
+
+
+// Add searched-for cities to the history (in local storage and to the page)
+function addToHistory(city) {
+
+    //Capitalize city name, done this way in case a city name has multiple words
+    let searchedForCity = city.toLowerCase();
+    let arr = searchedForCity.split(" ");
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    }
+    let formattedCityName = arr.join(" ");
+
+    if (historyItems.includes(formattedCityName)) {
+        searchInputEl.value = "";
+        return;
+    } else {
+        searchInputEl.value = "";
+
+        historyItems.push(formattedCityName);
+        numCities++;
+
+        localStorage.setItem("history", JSON.stringify(historyItems));
+
+        populateHistoryList();
+    }
+}
+
+
+function populateHistoryList() {
+    if (historyItems.length === 0) {
+        clearHistoryButton.disabled = true;
+        return;
+    } else {
+        // Remove all list items for ease of formatting local storage variables and populating list with loop
+        historyListEl.innerHTML = "";
+
+        for (let i = 0; i < historyItems.length; i++) {
+            let nextListItemEl = document.createElement("li");
+            nextListItemEl.textContent = historyItems[i];
+            nextListItemEl.classList.add("history-item");
+            historyListEl.appendChild(nextListItemEl);
+
+            // Add event listener to each history item
+            nextListItemEl.addEventListener("click", searchForCity);
+        }
+        clearHistoryButton.disabled = false;
+    }
+}
+
+function searchForCity(event) {
+    event.preventDefault();
+    console.log(event.target.textContent);
+} 
+
+populateHistoryList();
